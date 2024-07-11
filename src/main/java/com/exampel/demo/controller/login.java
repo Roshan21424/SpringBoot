27c6003ecf671d5b830a1a37763c0a8ce6900af3
RoshanRaj-M <roshanraj2142004@gmail.com>
2024-07-12 00:36:47 +0530

    adding configuration in application.properties file

--- src/main/java/com/exampel/demo/controller/login.java
+++ src/main/java/com/exampel/demo/controller/login.java
@@ -1,5 +1,6 @@
 package com.exampel.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class login {
 
+    @Value("${welcome.property}")
+    private String message;
+
     //setting endpointa
 
     //@RequestMapping(value="/",method= RequestMethod.GET)
     @GetMapping("/")
-    public String Login() {
-        return "welcome  to login";
+    public String Login()
+    {
+        return message;
     }
 }
